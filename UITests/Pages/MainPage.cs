f7f5b5336d4fffedffaaa18f21c75815c6ad72d9
IlshatIbrahimov <ilshat.ibrahimov721@gmail.com>
2021-03-01 15:19:46 +0300

    projects tests; add login method to utilities

---
+++ UITests/Pages/MainPage.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UITests.Pages
+{
+    public class MainPage : WebPage
+    {
+        public readonly By addProjectButton = By.XPath("//button[text()='Add project ']");
+        public readonly By projectsListItem = By.XPath("//ul[@class='sidebar__list sidebar__list--project scroll']//li//a");
+
+        public MainPage(BaseDriver baseDriver) : base(baseDriver, "")
+        {
+
+        }
+
+        public void CreateProject(string projectName)
+        {
+            Driver.Click(addProjectButton);
+            Driver.FillField(By.XPath("//input[@id='projectName']"), projectName);
+            Driver.Click(By.XPath("//button[text()='Create ']"));
+        }
+
+        public void AssertProjectExists(string projectName)
+        {
+            var projects = Driver.GetElements(projectsListItem);
+
+            bool isContains = projects.Any(p => p.Text.Equals(projectName));
+            Assert.True(isContains);
+        }
+    }
+}
---
+++ UITests/Suites/Projects/Projects.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using UITests.Pages;
+
+namespace UITests.Suites.Projects
+{
+    [Parallelizable]
+    [TestFixture]
+    [AllureParentSuite("Модуль \"Проекты\"")]
+    [AllureSuite("Проекты")]
+    [Category("All")]
+    [Category("Projects")]
+    [AllureNUnit]
+    class Projects : SuiteTemplate
+    {
+        private MainPage page;
+        private string projectName;
+
+        [OneTimeSetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            page = new MainPage(fixture.Driver);
+            Utilities.Login(fixture.Driver);
+        }
+
+        [TestCase(Description = "Создание проекта (Успех)")]
+        public void CreateProjectSuccess()
+        {
+            projectName = "Autotest project " + DateTime.Now;
+            
+            page.OpenPage();
+            page.CreateProject(projectName);
+
+            Thread.Sleep(2000);
+
+            page.AssertProjectExists(projectName);
+        }
+    }
+}
