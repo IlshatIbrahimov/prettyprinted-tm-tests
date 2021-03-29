45aecc6d79d7e38e6ae286803b17d02200099145
IlshatIbrahimov <ilshat.ibrahimov721@gmail.com>
2021-03-29 19:29:49 +0300

    edit and delete tests

--- FuncTests/Requests/TasksRequests.cs
+++ FuncTests/Requests/TasksRequests.cs
@@ -38,9 +38,68 @@ namespace FuncTests.Requests
             return JObject.Parse(Client.client.Execute(request).Content);
         }
 
+        public JObject EditTask(int projectId, string taskName,
+            string newTaskName = null, string content = null, int? priority = null,
+            int? status = null, int? type = null)
+        {
+            var taskInfo = GetTaskInfo(projectId, taskName);
+
+            RestRequest request = new RestRequest("/task", Method.PUT);
+            Client.Authorize(Utilities.TestUserEmail, Utilities.TestUserPassword);
+            Client.AddAuthorizationToken(request);
+
+            JObject requestJObject = new JObject();
+            requestJObject.Add("id", taskInfo.Value<int>("id"));
+
+            if (newTaskName != null)
+                requestJObject.Add("name", newTaskName);
+            else
+                requestJObject.Add("name", taskInfo.Value<String>("name"));
+
+            if (content != null)
+                requestJObject.Add("content", content);
+            else
+                requestJObject.Add("content", taskInfo.Value<String>("content"));
+
+            requestJObject.Add("assigneeId", taskInfo["assignee"].Value<int>("id"));
+
+            if (priority != null)
+                requestJObject.Add("priorityId", priority);
+            else
+                requestJObject.Add("priorityId", taskInfo["priority"].Value<int>("id"));
+
+            if (status != null)
+                requestJObject.Add("statusId", status);
+            else
+                requestJObject.Add("statusId", taskInfo["status"].Value<int>("id"));
+
+            if (type != null)
+                requestJObject.Add("typeId", type);
+            else
+                requestJObject.Add("typeId", taskInfo["type"].Value<int>("id"));
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddParameter("application/json", requestJObject, ParameterType.RequestBody);
+
+            return JObject.Parse(Client.client.Execute(request).Content);
+
+        }
+
+        public JObject DeleteTask(int projectId, string taskName)
+        {
+            int taskId = GetTaskId(projectId, taskName);
+
+            RestRequest request = new RestRequest($"/task/{taskId}", Method.DELETE);
+            Client.Authorize(Utilities.TestUserEmail, Utilities.TestUserPassword);
+            Client.AddAuthorizationToken(request);
+
+            return JObject.Parse(Client.client.Execute(request).Content);
+        }
+
         public JObject GetTaskInfo(int projectId, string taskName)
         {
             RestRequest request = new RestRequest($"/project/{projectId}", Method.GET);
+            Client.Authorize(Utilities.TestUserEmail, Utilities.TestUserPassword);
             Client.AddAuthorizationToken(request);
 
             var response = Client.client.Execute(request);
